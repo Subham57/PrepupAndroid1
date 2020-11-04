94ed828ab781cb11a67dc8874ef97ab2e6ead4da
SUBHAM BHUYAN <subhambhuyan57@gmail.com>
2020-11-05 00:36:38 +0530

    Work on profile of the student deshbord

--- app/src/main/java/com/subham/prepupfinal/StudentProfile.java
+++ app/src/main/java/com/subham/prepupfinal/StudentProfile.java
@@ -16,6 +16,8 @@ public class StudentProfile extends AppCompatActivity {
     private Button btn_update;
     private Button btn_change;
 
+    LoginResponse loginResponse = new LoginResponse();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +32,20 @@ public class StudentProfile extends AppCompatActivity {
         btn_update = findViewById(R.id.btn_update);
         btn_change = findViewById(R.id.btn_change);
 
-        LoginResponse loginResponse = new LoginResponse();
-        tvname.setText(loginResponse.userDetails.getFname());
-        tvphone.setText((int) loginResponse.userDetails.getPhnumber());
-        tvemail.setText(loginResponse.userDetails.getEmailid());
-        tvinst.setText(loginResponse.userDetails.getInstname());
-        tvrole.setText(loginResponse.userDetails.getRole());
+        loginResponse = (LoginResponse) getIntent().getSerializableExtra("userDetails");
+//        System.out.println(loginResponse.userDetails.getEmailid());
+        String fname = loginResponse.userDetails.getFname();
+        String mname = loginResponse.userDetails.getMname();
+        String lname = loginResponse.userDetails.getLname();
+        String name = fname+" "+mname+" "+lname;
+        long phone = loginResponse.userDetails.getPhnumber();
+        String email = loginResponse.userDetails.getEmailid();
+        String inst = loginResponse.userDetails.getInstname();
+        String role = loginResponse.userDetails.getRole();
+        tvname.setText(name);
+        tvphone.setText(String.valueOf(phone));
+        tvemail.setText(email);
+        tvinst.setText(inst);
+        tvrole.setText(role);
     }
 }
